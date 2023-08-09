07a0133b025d13f34e523b20af264234c185eb32
euphyfzhang <98727789+euphyfzhang@users.noreply.github.com>
2023-08-08 23:24:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import snowflake.connector
-import pandas
+import pandas, request
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -12,3 +12,8 @@ streamlit.text(my_data_row)
 my_cur.execute("SELECT * FROM ACCESS_CHECK;")
 second_table = my_cur.fetchall()
 streamlit.dataframe(second_table)
+
+streamlit.header('Toronto Crimes - Major Crimes Indicator')
+
+mci_json = pandas.json_normalize(https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Major_Crime_Indicators_Open_Data/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson)
+streamlit.dataframe(mci_json)
