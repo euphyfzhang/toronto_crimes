825605d748635d48af610f50d9871c1f5c933e9a
euphyfzhang <98727789+euphyfzhang@users.noreply.github.com>
2023-08-25 17:01:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_cur.execute("SELECT * FROM ACCESS_CHECK;")
 second_table = my_cur.fetchall()
 streamlit.dataframe(second_table)
 
-streamlit.header('Toronto Crimes - Major Crimes Indicator')
+streamlit.header('Toronto Crimes - Major Crimes Indicator hello world')
 
 mci_response = requests.get("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Major_Crime_Indicators_Open_Data/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
 mci_json = pandas.json_normalize(mci_response.json())
